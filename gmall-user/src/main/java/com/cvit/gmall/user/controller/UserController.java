889e0fa11137073f190565e81b024e67647cea21
MsCyan <1760510355@qq.com>
2019-12-03 14:15:53 +0800

    提交测试

---
+++ gmall-user/src/main/java/com/cvit/gmall/user/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.cvit.gmall.user.controller;
+
+public class UserController {
+}
---
+++ gmall-user/src/main/java/com/cvit/gmall/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,6 @@
+package com.cvit.gmall.user.service.impl;
+
+import com.cvit.gmall.user.service.UserService;
+
+public class UserServiceImpl implements UserService {
+}
